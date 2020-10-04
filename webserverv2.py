94604c3ef50240662f69bd89fd22639422c5d40b
GeorgeSandu <dan.george.sandu@gmail.com>
2020-10-03 20:11:13 -0400

    Add files via upload

--- webserverv2.py
+++ webserverv2.py
@@ -14,18 +14,24 @@ def webServer(port=13331):
     while True:
         #Establish the connection
         print('Ready to serve...')
-        connectionSocket, addr = #Fill in start      #Fill in end
+        connectionSocket, addr = serverSocket.accept()#Fill in start      #Fill in end
         try:
-            message = #Fill in start    #Fill in end
+            message = connectionSocket.recv(1024)#Fill in start    #Fill in end
             filename = message.split()[1]
-            f = open(filename[1:])
-            outputdata = #Fill in start     #Fill in end
+            
+			
+			f = open(filename[1:])
+            outputdata = f.read()#Fill in start     #Fill in end
+			f.close()
 
             #Send one HTTP header line into socket
+			connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
             #Fill in start
+			connectionSocket.send(outputdata)
 
-            #Fill in end
-
+          
+			connectionSocket.close()
+			
             #Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
@@ -34,14 +40,12 @@ def webServer(port=13331):
             connectionSocket.close()
         except IOError:
             #Send response message for file not found (404)
-            #Fill in start
-
-            #Fill in end
-
-            #Close client socket
-            #Fill in start
-
-            #Fill in end
+			print('404 Error: The File Was Not Found')
+			
+			connectionSocket.close()	
+			
+            
+		
 
     serverSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
